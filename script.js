5e3a420c7c8db4b09d095a03c59b7d5bf22732ce
YAP EN THONG <149508420+enthongy@users.noreply.github.com>
2025-04-30 13:46:17 +0000

    Pending changes exported from your codespace

---
+++ script.js
@@ -0,0 +1,9 @@
+function goToPage(page) {
+    window.location.href = page;
+  }
+  
+function scrollCarousel(direction) {
+  const carousel = document.getElementById('carousel');
+  const scrollAmount = 300;
+  carousel.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
+}
